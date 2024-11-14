ca2bd335a19a6f9b0e2f82494a4bc370ce19bac0
Anthony Suarez <asuarezop@gmail.com>
2024-11-13 23:09:45 -0500

    Created initial folder structure for Spring Boot app, made DB cluster connection to application

---
+++ src/main/java/com/pluralsight/deli/DeliShopApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight.deli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliShopApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliShopApp.class, args);
+	}
+
+}
