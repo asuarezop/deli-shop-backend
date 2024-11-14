4d18c8cc4f698458c14768b300b8db5d1ec59074
Anthony Suarez <asuarezop@gmail.com>
2024-11-14 08:52:01 -0500

    Fixed connection issues with using env variables in spring boot application by switching to a new one

--- src/main/java/com/pluralsight/deli/app/DeliShopApp.java
+++ src/main/java/com/pluralsight/deli/app/DeliShopApp.java
@@ -1,4 +1,4 @@
-package com.pluralsight.deli;
+package com.pluralsight.deli.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
