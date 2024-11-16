69d777259eb9c1ce37d70cbe571fc693f229ea0b
Anthony Suarez <asuarezop@gmail.com>
2024-11-15 23:48:50 -0500

    Added extra annotations to OrderItem() to create a one to many relationship between Order() and OrderItem(), successfully returned all orders when hitting GET api endpoint

--- src/main/java/com/pluralsight/deli/Order.java
+++ src/main/java/com/pluralsight/deli/Order.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -20,5 +21,7 @@ public class Order {
     private int orderId;
     private String orderDate;
     private String orderTime;
+
+    @DocumentReference
     private List<OrderItem> orderItems;
 }
--- src/main/java/com/pluralsight/deli/OrderItem.java
+++ src/main/java/com/pluralsight/deli/OrderItem.java
@@ -1,10 +1,15 @@
 package com.pluralsight.deli;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Document(collection = "items")
 public class OrderItem {
+    public OrderItem() {
+    }
+
     @Autowired
     private String size;
     private String bread;
